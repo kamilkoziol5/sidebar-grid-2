bdc3f5964e7d60b3169d4bd3e3704fbd29d2d077
Kamil <kamilkoziol529@gmail.com>
2025-03-23 10:29:57 +0100

    responsice sidebar & small color changes

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const sidebar = document.querySelector(".sidebar");
 	const toogleBtn = document.getElementById("toogler-btn");
+	const hamburgerBtn = document.querySelector(".hamburger");
+	const menuIcon = document.getElementById("menuIcon"); 
 
 	toogleBtn.addEventListener("click", () => {
 		sidebar.classList.toggle("open");
 		toogleBtn.classList.toggle("rotate");
+	});
+
+	hamburgerBtn.addEventListener("click", () => {
+		sidebar.classList.toggle("collapsed");
+
+		
+		if (menuIcon.classList.contains("bx-menu")) {
+			menuIcon.classList.remove("bx-menu");
+			menuIcon.classList.add("bx-x");
+		} else {
+			menuIcon.classList.remove("bx-x");
+			menuIcon.classList.add("bx-menu");
+		}
 	});
 });
