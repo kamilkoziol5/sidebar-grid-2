8ea1988420ffb5d6b023a7953de390bc0b9f8603
Kamil <kamilkoziol529@gmail.com>
2025-03-28 18:04:44 +0100

    new height body on mobile

--- JAVASCRIPT/todoapp.js
+++ JAVASCRIPT/todoapp.js
@@ -1,33 +1,32 @@
-const inputTask = document.getElementById("input-task-field");
-const addBtn = document.getElementById("add-task-btn");
-const taskContainer = document.getElementById("task-container");
+document.addEventListener("DOMContentLoaded", () => {
+	const inputTask = document.getElementById("input-task-field");
+	const addButton = document.getElementById("add-task-btn");
+	const taskContainer = document.getElementById("task-container");
 
-function AddTask() {
-	if (inputTask.value.trim() === "") {
-		alert("You must write something!");
-		return;
-	} else {
-		let li = document.createElement("li");
-		li.innerHTML = inputTask.value;
-		taskContainer.appendChild(li);
-		let span = document.createElement("span");
-		span.innerHTML = "\u00d7";
-		li.appendChild(span);
+	function addTask() {
+		if (inputTask.value.trim() === "") {
+			alert("You must write something");
+			return;
+		} else {
+			let li = document.createElement("LI");
+			li.innerHTML = inputTask.value;
+			taskContainer.appendChild(li);
+			let span = document.createElement("SPAN");
+			span.innerHTML = "\u00d7";
+			li.appendChild(span);
+		}
+		inputTask.value = "";
+		saveData();
 	}
-	inputTask.value = "";
-	saveData();
-}
 
-addBtn.addEventListener("click", AddTask);
-inputTask.addEventListener("keydown", function (e) {
-	if (e.key === "Enter") {
-		AddTask();
-	}
-});
+	addButton.addEventListener("click", addTask);
+	inputTask.addEventListener("keydown", function (e) {
+		if (e.key === "Enter") {
+			addTask();
+		}
+	});
 
-taskContainer.addEventListener(
-	"click",
-	function (e) {
+	taskContainer.addEventListener("click", function (e) {
 		if (e.target.tagName === "LI") {
 			e.target.classList.toggle("checked");
 			saveData();
@@ -35,15 +34,14 @@ taskContainer.addEventListener(
 			e.target.parentElement.remove();
 			saveData();
 		}
-	},
-	false
-);
+	});
 
-function saveData() {
-	localStorage.setItem("data", taskContainer.innerHTML);
-}
+	function saveData() {
+		localStorage.setItem("data", taskContainer.innerHTML);
+	}
 
-function showTask() {
-	taskContainer.innerHTML = localStorage.getItem("data");
-}
-showTask();
+	function showTask() {
+		taskContainer.innerHTML = localStorage.getItem("data");
+	}
+	showTask();
+});
